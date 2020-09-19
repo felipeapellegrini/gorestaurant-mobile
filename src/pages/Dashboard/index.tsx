c5a652c77f31ba6451d5023c7b312f5013f55ac7
Felipe A Pellegrini <felipeapellegrini@gmail.com>
2020-09-19 18:28:19 -0300

    começando o desafio

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -59,7 +59,19 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // Load Foods from API
+      const response = await api.get('/foods', {
+        params: {
+          category_like: selectedCategory,
+          name_like: searchValue,
+        },
+      });
+
+      setFoods(
+        response.data.map((food: Food) => ({
+        ...food,
+        formattedPrice: formatValue(food.price),
+      })),
+      );
     }
 
     loadFoods();
@@ -67,7 +79,9 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadCategories(): Promise<void> {
-      // Load categories from API
+      const response = await api.get('/categories');
+
+      setCategories(response.data);
     }
 
     loadCategories();
